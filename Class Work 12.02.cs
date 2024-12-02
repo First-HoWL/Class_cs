03896b7eaf60e1358c15174ef142e92707215ae1
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2024-12-02 20:01:02 +0200

    Update Class Work 12.02.cs

--- Class Work 12.02.cs
+++ Class Work 12.02.cs
@@ -1,35 +1,7 @@
 // See https://aka.ms/new-console-template for more information
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
-
-class Character
-{
-    string? name;
-    int health;
-    int damage;
-    int defence;
-
-    public Character(string? name, int health, int damage, int defence)
-    {
-        this.name = name;
-        this.health = health;
-        this.damage = damage;
-        this.defence = defence;
-    }
-
-    public void print()
-    {
-        Console.WriteLine($"-< {name} >- ");
-        Console.WriteLine($" Hp: {health} ");
-        Console.WriteLine($" Damage: {damage} ");
-        Console.WriteLine($" Defence: {defence} ");
-    }
-}
-
+using Game;
+using System.Threading;
 class Program
 {
     static void Main(string[] args)
@@ -39,6 +11,70 @@ class Program
 
         Character player1 = new Character("HoWL", 100, 15, 4);
         player1.print();
+        Console.WriteLine();
+
+        Character player2 = new Character("Anton", 90, 18, 7);
+        player2.print();
+
+        while (true)
+        {
+            Console.WriteLine();
+            player2.attack(player1);
+            player1.print();
+
+            Thread.Sleep(2000);
+        }
+
+        
+    }
+}
+
+
+
+
+
+
+namespace Game
+{
+    class Character
+    {
+        string? name;
+        int health;
+        int damage;
+        int defence;
+
+        public Character(string? name, int health, int damage, int defence)
+        {
+            this.name = name;
+            this.health = health;
+            this.damage = damage;
+            this.defence = defence;
+        }
+
+        public void print()
+        {
+            Console.WriteLine($"-< {name} >- ");
+            Console.WriteLine($" Hp: {health} ");
+            Console.WriteLine($" Damage: {damage} ");
+            Console.WriteLine($" Defence: {defence} ");
+        }
+
+        public int take_damage(int damage_)
+        {
+            double damage = Convert.ToDouble(damage_);
+            Random? rand = new Random();
+            int a = rand.Next(0, 2);
+            if (a == 0)
+                damage -= (damage / 10);
+            else
+                damage += (damage / 10);
+            int dmg = Convert.ToInt32((damage - (damage / 100 * defence)));
+            this.health -= Math.Max(dmg, 0);
+            return dmg;
+        }
+        public void attack( Character target ) { 
+            target.take_damage(this.damage);
+        }
 
     }
 }
