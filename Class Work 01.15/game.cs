242350d1385b5930c0e72fe64245aa4878f4a3d1
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2025-01-15 20:48:23 +0200

    Add files via upload

---
+++ Class Work 01.15/game.cs
@@ -0,0 +1,95 @@
+﻿using System.Drawing;
+
+namespace Game.Game
+{
+    class Player
+    {
+        private Point pos;
+        public Point Pos { get { return pos; } set { pos.X = Math.Max(value.X, 0); pos.Y = Math.Max(value.Y, 0); } }
+        public ConsoleColor color = ConsoleColor.White;
+
+        public void Move(Point pos)
+        {
+            this.pos.X = Math.Min(Math.Max(this.pos.X + pos.X, 1), 100);
+            this.pos.Y = Math.Min(Math.Max(this.pos.Y + pos.Y, 1), 28);
+        }
+        public void Move(int x, int y)
+        {
+            this.Move(new Point(x, y));
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(pos.X, pos.Y);
+            Console.ForegroundColor = color;
+            Console.Write("██");
+            Console.ResetColor();
+        }
+        
+
+        public int Distanse(Point point)
+        {
+            return (int) Math.Sqrt(Math.Pow(this.pos.X - point.X, 2) + Math.Pow(this.pos.Y - point.Y, 2));
+        }
+
+    }
+    class Coin
+    {
+        private Point pos;
+        public Point Pos { get { return pos; } set { pos.X = Math.Max(value.X, 0); pos.Y = Math.Max(value.Y, 0); } }
+        public ConsoleColor color = ConsoleColor.Yellow;
+        public void Draw()
+        {            
+            Console.SetCursorPosition(pos.X, pos.Y);
+            Console.ForegroundColor = color;
+            Console.Write("🪙");
+            Console.ResetColor();
+        }
+        public Coin(int x, int y) : this(new Point(x, y)) { }
+        public Coin(Point pos) { this.pos = pos; }
+        public void Move(Point pos)
+        {
+            this.pos.X = Math.Min(Math.Max(this.pos.X + pos.X, 1), 100);
+            this.pos.Y = Math.Min(Math.Max(this.pos.Y + pos.Y, 1), 28);
+        }
+        public void Move(int x, int y)
+        {
+            this.Move(new Point(x, y));
+        }
+
+    }
+
+    class ScoreBar 
+    {
+        public int score;
+        public Point pos;
+        private int width = 15;
+        private int height = 3; 
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(pos.X, pos.Y);
+            for (int i = 0; i < height - 1; i++)
+            {
+                Console.SetCursorPosition(pos.X, pos.Y + i);
+                Console.Write("|");
+            }
+            
+            Console.SetCursorPosition(pos.X + 1, pos.Y + height - 1);
+            for (int i = 0; i < width - 1; i++)
+                Console.Write("_");
+            for (int i = 0; i < height - 1; i++)
+            {
+                Console.SetCursorPosition(pos.X + width, pos.Y + i);
+                Console.Write("|");
+            }
+            Console.SetCursorPosition(pos.X + width, pos.Y + height - 1);
+            Console.Write("\b/");
+            Console.SetCursorPosition(pos.X + 2, pos.Y + height - 1);
+            Console.Write("\b\\");
+            Console.SetCursorPosition(pos.X + (width / 2) , pos.Y + (height / 2) - (height % 2));
+            Console.Write(score);
+        }
+    }
+
+}
---
+++ Class Work 01.15/main.cs
@@ -0,0 +1,81 @@
+﻿using System.Text;
+using Game.Game;
+using System.Threading;
+using System;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            Console.InputEncoding = UTF8Encoding.UTF8;
+            Random rnd = new Random(DateTime.Now.Second);
+
+            Player player = new Player();
+            player.Pos = new Point(1, 3);
+
+
+            /*Player player2 = new Player();
+            player2.Pos = new Point(4, 3);
+            player2.color = ConsoleColor.Green;*/
+            
+            List<Coin> coinList = new List<Coin>();
+            for (int i = 0; i < 5; i++)
+                coinList.Add(new Coin(rnd.Next(1, 100), rnd.Next(1, 28)));
+
+            ScoreBar scoreBar = new ScoreBar();
+            scoreBar.pos = new Point(20, 0);
+            while (true) {
+                scoreBar.Draw();
+                foreach (Coin coin in coinList.ToList()) 
+                { 
+                    if (player.Distanse(coin.Pos) <= 1)
+                    {
+                        
+                        coinList.Remove(coin);
+                        scoreBar.score++;
+                        coinList.Add(new Coin(rnd.Next(1, 100), rnd.Next(1, 28)));
+                    }
+                    else
+                    {
+                        coin.Move(rnd.Next(-2, 2) >= 0 ? 1 : -1, rnd.Next(-2, 2) >= 0 ? 1 : -1) ;
+                        coin.Draw();
+                    }
+
+                }
+
+                player.Draw();
+                //player2.Draw();
+                
+
+                ConsoleKey key = Console.ReadKey().Key;
+                if (key == ConsoleKey.Delete) { Console.Clear();   break; }
+                if (key == ConsoleKey.W)
+                    player.Move(0, -1);
+                else if (key == ConsoleKey.A)
+                    player.Move(-1, 0);
+                else if (key == ConsoleKey.S)
+                    player.Move(0, 1);
+                else if (key == ConsoleKey.D)
+                    player.Move(1, 0);
+               /* else if (key == ConsoleKey.UpArrow)
+                    player2.Move(0, -1);
+                else if (key == ConsoleKey.LeftArrow)
+                    player2.Move(-1, 0);
+                else if (key == ConsoleKey.DownArrow)
+                    player2.Move(0, 1);
+                else if (key == ConsoleKey.RightArrow)
+                    player2.Move(1, 0);*/
+                Console.Clear();
+            }
+        }
+    }
+}
