15f01b1f6050b20af25bb0d49a664d49124f1a5d
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2025-01-29 18:53:37 +0200

    Update Program.cs

--- Class Work 01.29/Program.cs
+++ Class Work 01.29/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,21 +9,23 @@ using System.Text.Json;
 
 namespace HoWL_C_.Class_Work_01._27
 {
-    class Program {
+    class Program
+    {
         public static async Task<cardes> card(int count, string deck_id)
         {
             var client = new HttpClient();
             string url = $"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count={count}";
-            var response = client.GetAsync(url).Result;
+            var response = await client.GetAsync(url);
             if (response.IsSuccessStatusCode)
             {
-                var JsonResponse = response.Content.ReadAsStringAsync().Result;
+                var JsonResponse = await response.Content.ReadAsStringAsync();
                 return JsonSerializer.Deserialize<cardes>(JsonResponse);
             }
             return null;
         }
 
-        public static async Task Main(string[] args){
+        public static async Task Main(string[] args)
+        {
             Console.OutputEncoding = UTF8Encoding.UTF8;
             Console.InputEncoding = UTF8Encoding.UTF8;
             deck res = new deck(1);
@@ -31,17 +33,16 @@ namespace HoWL_C_.Class_Work_01._27
             Console.WriteLine(res.remaining);
             Console.WriteLine(res.cards);
             int r = 20;
-            var a = card(r, res.deck_id);
-            List <card> d = new List<card>();
-            for (int i = 0; i < r; i++)
-                d.Add(a.retCards(i));
+            cardes a = new cardes();
+            a = await card(r, res.deck_id);
+            
             if (a == null)
                 Console.WriteLine("error");
-            
-            else foreach (var card in d)
-            {
-                card.print();
-            }
+
+            else foreach (var card in a.cards)
+                {
+                    card.print();
+                }
             Console.WriteLine(res.remaining);
         }
     }
