35f24a0918be5cc7368d40e56b153a1a12f4ba2f
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2025-01-29 20:38:55 +0200

    Update Program.cs

--- Class Work 01.29/Program.cs
+++ Class Work 01.29/Program.cs
@@ -6,9 +6,51 @@ using System.Threading.Tasks;
 using DeckofCards;
 using System.Net.Http;
 using System.Text.Json;
+using System.Drawing;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace HoWL_C_.Class_Work_01._27
+namespace HoWL_C
 {
+
+    class API
+    {
+        public static readonly string host = "https://date.nager.at/";
+        public static readonly HttpClient httpClient = new HttpClient();
+
+        public static string MakeRequest(string url)
+        {
+            var responce = httpClient.GetAsync(url).Result;
+            if (responce != null && responce.IsSuccessStatusCode)
+            {
+                return responce.Content.ReadAsStringAsync().Result;
+            }
+            else throw new HttpRequestException();
+        }
+    }
+    enum HolidayTypes
+    {
+        Public, Bank, School, Authorities, Optional, Observance
+    }
+    class Holiday
+    {
+        public string date { get; set; }
+        public string? localName { get; set; }
+        public string? name { get; set; }
+        public string? countryCode { get; set; }
+
+        public bool global { get; set; }
+        public int? launchYear { get; set; }
+        public List<string> types { get; set; }
+
+        public override string ToString()
+        {
+            
+            return $"{localName} - {date}";
+        }
+    }
+
+    
+
     class Program
     {
         public static async Task<cardes> card(int count, string deck_id)
@@ -24,9 +66,11 @@ namespace HoWL_C_.Class_Work_01._27
             return null;
         }
 
+
         public static async Task Main(string[] args)
         {
-            Console.OutputEncoding = UTF8Encoding.UTF8;
+
+            /*Console.OutputEncoding = UTF8Encoding.UTF8;
             Console.InputEncoding = UTF8Encoding.UTF8;
             deck res = new deck(1);
             Console.WriteLine(res.deck_id);
@@ -35,7 +79,7 @@ namespace HoWL_C_.Class_Work_01._27
             int r = 20;
             cardes a = new cardes();
             a = await card(r, res.deck_id);
-            
+
             if (a == null)
                 Console.WriteLine("error");
 
@@ -43,7 +87,39 @@ namespace HoWL_C_.Class_Work_01._27
                 {
                     card.print();
                 }
-            Console.WriteLine(res.remaining);
+            Console.WriteLine(res.remaining);*/
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            Console.InputEncoding = UTF8Encoding.UTF8;
+
+            string url = "https://date.nager.at/api/v3/PublicHolidays/2025/UA";
+            string n = API.MakeRequest(url);
+            List<Holiday> g = new List<Holiday>();
+            g = JsonSerializer.Deserialize<List<Holiday>>(n);
+            
+            if (g == null)
+                Console.WriteLine("erorr");
+            bool next = true;
+            foreach(var Hollyday in g)
+            {
+                
+                if (DateTime.ParseExact(Hollyday.date, "yyyy-MM-dd", null) < DateTime.Now)
+                    Console.ForegroundColor = ConsoleColor.Red;
+                
+                else if (DateTime.ParseExact(Hollyday.date, "yyyy-MM-dd", null) >= DateTime.Now && next) {
+                    Console.WriteLine($"Today - {DateTime.Now}");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    next = false;
+                }
+                
+                else
+                    Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(Hollyday);
+                Console.ResetColor();
+            }
+             
+
+            
+
         }
     }
 
