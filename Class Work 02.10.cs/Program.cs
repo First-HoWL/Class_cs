2a8f1647eafedf474e14fb241f466331d3b89a83
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2025-02-10 20:53:15 +0200

    Create Program.cs

---
+++ Class Work 02.10.cs/Program.cs
@@ -0,0 +1,37 @@
+using System.Text;
+using System.Threading;
+using System;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using Game;
+
+namespace SnakeGame
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            Console.InputEncoding = UTF8Encoding.UTF8;
+            // https://github.com/svitlyi-itstep/CSH_P35/blob/master/CSH_P35/exam.md
+            Games game = new Games();
+
+            game.StartGame();
+            ConsoleKey key;
+            while (true)
+            {
+                do
+                {
+                     key = Console.ReadKey(true).Key;
+                } while (game.InputHandler(key) == false);
+                Console.Clear();
+            }
+        }
+    }
+}
